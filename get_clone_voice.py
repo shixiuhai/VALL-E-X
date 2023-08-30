d1d9712824faf6bc03ad1608362349d4e5930bb3
shixiuhai <15256728901@163.com>
2023-08-31 00:19:20 +0800

    提交了修改配置

--- get_clone_voice.py
+++ get_clone_voice.py
@@ -1,10 +1,10 @@
 import requests
 
-url = 'http://127.0.0.1:7000/textToVoice'  # 修改为您的API接口URL
+url = 'http://124.222.114.154:7500/textToVoice'  # 修改为您的API接口URL
 
 params = {
-    'text': "体验超凡的口腔清洁与舒适，我们自豪地推出全新的维尔牌高级抗菌电动牙刷。以先进的抗菌技术为基础，配备高效振动清洁，能轻松去除牙菌斑、污渍和食物残渣。多重清洁模式，智能提醒功能，以及持久续航，为您的口腔健康保驾护航。时尚外观和舒适握持设计，让您享受每一次刷牙的愉悦体验。选择维尔，迎接健康与自信的笑容！",
-    'promptName': "dbd206bf-2ddb-40f3-962c-879ea05d656c"
+    'text': "体验超凡的口腔清洁与舒适",
+    'promptName': "3fabef64-7986-434e-ae3b-ff36c424a8ae"
 }
 
 response = requests.post(url, json=params)
---
+++ nohup_start.sh
@@ -0,0 +1,2 @@
+# nohup python my_script.py > /dev/null 2>&1 &
+nohup python main.py &
